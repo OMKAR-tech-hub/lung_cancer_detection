39855e76e0b94385b3c2764a0c0acec7bf8b751a
OMKAR <omgaidankar07@gmail.com>
2025-12-07 00:32:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from tensorflow.keras.preprocessing import image
 from streamlit_option_menu import option_menu
 st.set_page_config(page_title='Lung Cancer Detection')
 #Loading models
-cancer_model = load_model('models/keras_model.h5')
+cancer_model = load_model('models/final_model.sav','rb')
 
 
 with st.sidebar:
